ccf5f90802f2c81fba53321c5b00427d96e00264
w1f666 <wyfinfinity@outlook.com>
2026-02-18 15:39:47 +0800

    month获取json-server数据

--- my-router-app/src/pages/Month/Month.tsx
+++ my-router-app/src/pages/Month/Month.tsx
@@ -1,4 +1,17 @@
+import type { RootState } from "@/store";
+import { useSelector } from "react-redux";
+
 const Month: React.FC = () => {
-  return  <div>我是month组件</div>
+  const billList = useSelector((state: RootState) => state.bill.billList);
+  return (<div>
+    {billList.map((bill: any) => (
+      <div key={bill.id}>
+        <p>金额: {bill.money}</p>
+        <p>类型: {bill.type}</p>
+        <p>日期: {bill.date}</p>
+      </div>
+    ))
+    }
+    </div>)
 }
 export default Month;
